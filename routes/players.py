e904f43a78383d543757ffdf3ee4f37d69c8be96
Pongstr <twiz.tickler@gmail.com>
2024-06-12 23:11:08 +0300

    .

--- routes/players.py
+++ routes/players.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 
 from modules.db import database
 from modules.players import Players
@@ -7,17 +7,21 @@ app = Blueprint("players", __name__, url_prefix="/players")
 players_db = Players(database)
 
 
+def error_handler(code: int, msg: str):
+    return jsonify({"status": str(code), "message": msg}, code)
+
+
 @app.get("/")
 def players():
     args = request.args if not None else None
     return players_db.get_players(args)
 
 
+@app.post("/")
+def find_player():
+    return players_db.find_player(request)
+
+
 @app.get("/<player_slug>")
 def player(player_slug):
     return players_db.get_player(player_slug)
-
-
-@app.post("/find")
-def find_player(keyword):
-    return players_db.find_player(keyword)
